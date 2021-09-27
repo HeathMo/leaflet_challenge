a269a93ade7a1db4ccf6fdfe9f3cee8111081efd
HeathMo <heather.mott@yahoo.com>
2021-09-26 22:03:13 -0400

    start

---
+++ Leaflet-Step-1/git.ignore/config.js
@@ -0,0 +1,5 @@
+// Visualizations
+
+var earthquakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
+var platesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+
